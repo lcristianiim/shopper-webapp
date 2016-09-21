44b03a0c81fcba4f5ef797954b75f15bdd0a8161
lcristianiim <lcristianiim@gmail.com>
2016-09-21 12:26:28 +0300

    Added implementation methods for /<personName>/items

--- src/main/java/com/shopper/restControllers/PersonControllerImpl.java
+++ src/main/java/com/shopper/restControllers/PersonControllerImpl.java
@@ -1,10 +1,13 @@
 package com.shopper.restControllers;
 
+import com.shopper.models.Item;
 import com.shopper.models.Person;
 import com.shopper.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,5 +29,17 @@ public class PersonControllerImpl implements PersonController {
     public ResponseEntity savePerson(@RequestBody List<Person> persons) {
         List<Person> addedPersons = personService.save(persons);
         return ResponseEntity.ok().body(addedPersons);
+    }
+
+    @Override
+    public ResponseEntity getPersonsItems(@PathVariable("personName") String personName) {
+        List<Item> personsItems = personService.getAllItems(personName);
+        return ResponseEntity.ok().body((List<Item>) personsItems);
+    }
+
+    @Override
+    public ResponseEntity savePersonsItem(@PathVariable("personName") String person, @RequestBody List<Item> items) {
+        Person addedPerson = personService.saveItems(person, items);
+        return ResponseEntity.ok().body(addedPerson);
     }
 }
